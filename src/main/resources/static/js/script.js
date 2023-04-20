94ee812ea4321730118a418b1dd5083d4c534af0
eattato <80892781+eattato@users.noreply.github.com>
2023-04-20 14:39:32 +0900

    차트 2개 만듬

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,5 @@
 let data = {};
+let charts = {};
 
 const getAverage = (list) => {
     let sum = 0;
@@ -10,7 +11,7 @@ const getAverage = (list) => {
 
 const getLocationDataAverage = (location) => {
     let locationData = [];
-    if (location) {
+    if (location && location != "all") {
         locationData = data[location];
     } else {
         for (let loc in data) {
@@ -57,6 +58,22 @@ const getColumn = (data, column) => {
     return result;
 }
 
+const addOptions = (select, options) => {
+    for (let i in options) {
+        let option = options[i];
+        let element = $($.parseHTML("<option></option>"));
+        element.text(option);
+        element.val(option);
+        select.append(element);
+    }
+}
+
+const destroyChart = (element) => {
+    if (charts[element]) {
+        charts[element].destroy();
+    }
+}
+
 fetch("/data/weather3.json")
 .then(res => res.json())
 .then((res) => {
@@ -67,24 +84,90 @@ fetch("/data/weather3.json")
     let rainMonthChart = $("#rain_month");
     let ratioChart = $("#weather_ratio");
 
-    let globalData = getLocationDataAverage();
-    console.log(globalData);
-    // console.log(getColumn(globalData, "tempurature"));
-    // console.log(getColumn(globalData, "humidity"));
-    new Chart(dailyChart.find(".chart_body"), {
-        type: "line",
-        data: {
-            // labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
-            datasets: [
-                {
-                    label: "기온",
-                    data: getColumn(globalData, "tempurature"),
-                },
-                {
-                    label: "습도",
-                    data: getColumn(globalData, "humidity")
-                }
-            ]
+    addOptions(dailyChart.find("select"), Object.keys(data));
+    addOptions(ratioChart.find("select"), Object.keys(data));
+
+    function updateDailyChart(val) {
+        let element = dailyChart;
+        destroyChart(element.attr("id"));
+
+        let dailyData = getLocationDataAverage(val);
+        charts[dailyChart.attr("id")] = new Chart(dailyChart.find(".chart_body"), {
+            type: "line",
+            data: {
+                labels: Object.keys(dailyData),
+                datasets: [
+                    {
+                        label: "기온",
+                        data: getColumn(dailyData, "tempurature"),
+                        borderColor: "rgb(150, 150, 0)",
+                        showLine: true,
+                        spanGaps: true,
+                        pointRadius: 0,
+                        borderWidth: 0.75
+                    },
+                    {
+                        label: "습도",
+                        data: getColumn(dailyData, "humidity"),
+                        borderColor: "rgb(70, 70, 255)",
+                        showLine: true,
+                        spanGaps: true,
+                        pointRadius: 0,
+                        borderWidth: 0.75
+                    }
+                ]
+            }
+        })
+    }
+
+    function updateWeatherRatio(val) {
+        let element = ratioChart;
+        destroyChart(element.attr("id"));
+
+        let locationData = getLocationDataAverage(val);
+        let rainCount = 0;
+        let snowCount = 0;
+        let sunnyCount = 0;
+        for (let date in locationData) {
+            let dayData = locationData[date];
+            if (dayData.snow > 0) {
+                snowCount += 1;
+            } else if (dayData.rain >= 0.5) {
+                rainCount += 1;
+            } else {
+                sunnyCount += 1;
+            }
         }
-    })
+
+        charts[element.attr("id")] = new Chart(element.find(".chart_body"), {
+            type: "pie",
+            data: {
+                labels: ["맑음", "비", "눈"],
+                datasets: [{
+                    label: "날씨 비율",
+                    data: [sunnyCount, rainCount, snowCount],
+                    backgroundColor: [
+                        "#FFF04D",
+                        "#1E85E6",
+                        "#EEEEEE"
+                    ]
+                }]
+            }
+        })
+    }
+
+    // 업데이트
+    dailyChart.find("select").change(function() {
+        let val = $(this).val();
+        updateDailyChart(val);
+    });
+
+    ratioChart.find("select").change(function() {
+        let val = $(this).val();
+        updateWeatherRatio(val);
+    });
+
+    // 기본 표시
+    updateDailyChart("all");
+    updateWeatherRatio("all");
 });
