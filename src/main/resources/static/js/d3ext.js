eaa209151112e2cd9e8126b034c65ec0a1f29e56
eattato <80892781+eattato@users.noreply.github.com>
2023-05-04 09:36:21 +0900

    가로 bar 차트 그리는데 성공함

--- src/main/resources/static/js/d3ext.js
+++ src/main/resources/static/js/d3ext.js
@@ -9,8 +9,8 @@ export const horizontalBar = (element, data, option) => {
     */
 
     let d3Element = d3.select(element[0]);
-    let y = data.labels;
     let x = data.values;
+    let y = data.labels;
 
     let width = element.width();
     let height = element.height();
@@ -18,31 +18,47 @@ export const horizontalBar = (element, data, option) => {
     // 스케일링
     let axisPadding = 20;
     let axisSize = 20;
+    let barSize = 0.75;
 
-    let xScale = d3.scaleLinear() // 파라미터를 넣으면 0 ~ max의 범위에서 range에 맞게 스케일해서 리턴하는 함수를 받음
-        // .domain([d3.min(x), d3.max(x)])
+    /**
+     * 파라미터(x값)를 넣으면 0 ~ max의 범위에서 스케일링하는 함수
+     * 시작: 패딩 + y 액시스 공간, 끝: full - 패딩
+     * @param {number} x 해당 값을 차트 위치 & 사이즈에 맞게 스케일링
+     * @returns range에 맞게 스케일링해서 width를 리턴
+     */
+    let xScale = d3.scaleLinear()
         .domain([0, d3.max(x)])
-        .range([axisPadding + axisSize, width - axisPadding]); // 시작: 패딩 + y 액시스 공간, 끝: full - 패딩
+        .range([axisPadding + axisSize, width - axisPadding]);
 
+    /**
+     * 파라미터(y값, 라벨)을 넣으면 라벨의 순서에 따라 스케일링하는 함수
+     * 시작: 패딩, 끝: full - 패딩 - x 액시스 공간
+     * @param {string} y 해당 라벨 위치를 사용해 차트 위치 & 사이즈에 맞게 스케일링
+     * @returns range에 맞게 스케일링해서 y 위치를 리턴
+     */
     let yScale = d3.scaleBand()
         .domain(y)
-        .range([axisPadding, height - axisPadding - axisSize]) // 시작: 패딩, 끝: full - 패딩 - x 액시스 공간
+        .range([axisPadding, height - axisPadding - axisSize]) // 
 
     // 축 생성
     let xAxisFrame = d3Element.append("g")
-        .attr("transform", `translate(0, ${height - axisSize - axisPadding})`);
+        .attr("transform", `translate(0, ${height - axisSize - axisPadding})`); // 위에 생성되니까 y위치를 끝 - y 액시스 공간 - 패딩으로 설정
     let yAxisFrame = d3Element.append("g")
-        .attr("transform", `translate(${axisSize + axisPadding}, 0)`);
+        .attr("transform", `translate(${axisSize + axisPadding}, 0)`); // 패딩과 액시스 사이즈 적용
     let xAxis = d3.axisBottom(xScale)
     let yAxis = d3.axisLeft(yScale);
     xAxis(xAxisFrame);
     yAxis(yAxisFrame);
 
     // 데이터 표시
-    // d3Element.data(x)
-    //     .enter()
-    //     .append("rect")
-    //     .attr("width", () => )
-    //     .attr("x", axisPadding + axisSize)
-    //     .attr("y", (v, i) => {})
+    let heightResized = yScale.bandwidth() * barSize; // height는 y 스케일의 한 칸 사이즈 * bar 사이즈 배율로
+    d3Element.selectAll("rect")
+        .data(x) // x 데이터를 사용
+        .enter()
+        .append("rect") // 데이터 갯수에 맞춰 사각형 생성
+        .attr("width", (v) => xScale(v) - axisPadding - axisSize) // width는 각 데이터(foreach를 통해 x의 요소인 v를 얻음) 값을 xScale에 돌린거에 왼쪽으로 민 만큼 뺌
+        .attr("height", heightResized) // height는 미리 설정한 오프셋으로 설정
+        .attr("x", axisPadding + axisSize) // 패딩 + 액시스 사이즈 위치만큼 왼쪽으로 밈
+        .data(y) // 이후 사용할 데이터를 y 데이터로 변경
+        .attr("y", (v) => yScale(v) + (yScale.bandwidth() - heightResized) / 2) // 각 라벨(v)을 yScale에 돌리고 조정된 height에 맞춰 y위치를 설정
 }
